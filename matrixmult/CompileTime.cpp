2b2afcc5197368544d4266f1cf14f648a2c0d581
bradfirj92 <bradfirj92@gmail.com>
2013-02-07 13:53:09 +0000

    CompileTime.cpp: Result now computed at compile time, TODO: build string at compile time. Makefile: ":>" creates if not exists, truncates file to zero if exists. TestData.h: changed arrays from const -> constexpr
    From: William Stevenson <yhvh2000@gmail.com>

--- matrixmult/CompileTime.cpp
+++ matrixmult/CompileTime.cpp
@@ -1,24 +1,47 @@
 #include <chrono>
 #include <iostream>
+#include <stdio.h>
 #include "TestHelpers.h"
 #include "TestData.h"
-#include "prettyprint.hpp"
 using namespace std;
 
-constexpr int mMult(const int m, const int n) {
-  return m * n;
+// http://stackoverflow.com/questions/12108390/c11-compile-time-calculation-of-array
+
+typedef array<int, SIZE> matrix;
+
+constexpr int f(const int a) { return matrix_A[a] * matrix_B[a]; }
+
+template<int... i>
+constexpr matrix fs() {
+  return matrix{{ STRINGIFICATOR(f(i)...) }};
 }
+
+template<int... i> struct S;
+
+template<int... i> struct S<0, i...> {
+  static constexpr matrix gs() {
+    return fs<0, i...>();
+  }
+};
+template<int i, int... j> struct S<i, j...> {
+  static constexpr matrix gs() {
+    return S<i-1, i, j...>::gs();
+  }
+};
+
+constexpr auto result = S<SIZE-1>::gs();
+
 
 int main(int argc, char *argv[])
 {
   TIMER_START
 
-  int result[SIZE];
+  // TODO: Create string at compile time.
 
-  for (int i = 0; i < SIZE; ++i)
-    result[i] = mMult(c_matrix_A[i], c_matrix_B[i]);
-
-cout << result << endl;
+  for (int i = 0; i < SIZE ; ++i) {
+    printf("%d",result[i]);
+  }
+  puts("\n");
 
   TIMER_END
   return 0;
